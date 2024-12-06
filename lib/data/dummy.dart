40e6846fd969410fff529cea19053be05d4b6f97
Azza Wafiqurrohmah <wafiqurrohmahazza@gmail.com>
2024-12-06 14:52:13 +0700

    fix code

--- lib/data/dummy.dart
+++ lib/data/dummy.dart
@@ -1,3 +1,6 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
 class ChoiceItem {
   String name;
   bool isChoice;
@@ -15,3 +18,70 @@ Map<String, dynamic> infoBloom = {
   'status': 0, //0 perlu disiram //1 perlu diberi cahaya //2 tanaman sedang baik
   'name': 'MyJasmine',
 };
+
+
+
+Future<void> updatestatus(BuildContext context, String device, String name) async {
+  DatabaseReference ref = FirebaseDatabase.instance.ref("devices/$device");
+
+  try {
+    final snapshot = await ref.get();
+    if (!snapshot.exists) {
+      await showAlertDialog(
+      context,
+      "Error",
+      "Perangkat dengan token tersebut tidak ditemukan.",
+    );
+    }
+
+    if (snapshot.exists) {
+      await ref.update({
+        "status" : 'active'
+      });
+
+      DatabaseReference refSet = FirebaseDatabase.instance.ref("iot/$device");
+      await refSet.set({
+        "name": name,
+        "notified": 0,
+        "humidity": 0,
+        "intensity": 0,
+        "temperature": 0,
+        "condition": "Aku sehat"
+      });
+      
+      await showAlertDialog(
+        context,
+        "Sukses",
+        "Perangkat Berhasil Dihubungkan",
+      );
+    }
+    print("Age updated successfully!");
+  } catch (error) {
+    await showAlertDialog(
+          context,
+          "Error",
+          "Terjadi kesalahan",
+        );
+    print("Failed to update age: $error");
+  }
+}
+
+Future<void> showAlertDialog(BuildContext context, String title, String message) async {
+  await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text("OK"),
+          ),
+        ],
+      );
+    },
+  );
+}
